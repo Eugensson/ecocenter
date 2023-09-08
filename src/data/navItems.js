3e4cd12becdfd1da3de262488812bc78d5d917c1
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2023-09-08 20:21:03 +0300

    Update navItems.js

--- src/data/navItems.js
+++ src/data/navItems.js
@@ -1,8 +1,8 @@
 export const navItems = [
     { label: 'Про ЕкоЦентр', href: '/' },
     { label: 'Екологічні послуги', href: '/services' },
-    { label: 'Лабораторія', href: '/technology' },
-    { label: 'Кваліфікація', href: '/assurance' },
+    { label: 'Лабораторія', href: '/laboratory' },
+    { label: 'Кваліфікація', href: '/qualification' },
     { label: 'Новини', href: '/news' },
     { label: 'Контакти', href: '/contacts' },
   ];
