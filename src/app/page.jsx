6496e7d23869a9cf3b08b9b199a40f3ff4d6826c
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2023-09-09 09:04:04 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <section>
+    <section className="sectionsWrapper">
       <h2>Головна сторінка</h2>      
     </section>
   )
