d805371ea98cb85ef612787c061f49b5d95481f8
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2023-09-11 23:32:22 +0300

    Update Contacts-page

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -18,13 +18,20 @@ const Contacts = () => {
     <section className="sectionContainer">
       <div className="bgiContainer bgiContacts"></div>
       <h1>Контакти</h1>
-      <p className={styles.text}><BusinessIcon/>18000, м. Черкаси, вул. Святотроїцька, 102/1, офіс: 3, 5</p>      
-      <Link className={styles.text} href="mailto:ncpnv21@ukr.net"><ContactMailIcon/>ncpnv21@ukr.net</Link>
-      <Link className={styles.text} href="tel:+380472361014"><FaxIcon/>+38-0472-361-014</Link>      
-      <Link className={styles.text} href="tel:+380472361014"><ContactPhoneIcon/>+38-0472-361-014</Link>      
-      <Link className={styles.text} href="tel:+380673956256"><PhoneIcon/>+38-067-395-62-56</Link>
-      <h2>Мапа проїзду</h2>
-      <Map/>
+      <div className="subSectionContainer">
+        <div className="contactContainer">
+          <p className="text"><BusinessIcon/>18000, м. Черкаси, вул. Святотроїцька, 102/1, офіс: 3, 5</p>
+          <Link className="text" href="mailto:ncpnv21@ukr.net"><ContactMailIcon/>ncpnv21@ukr.net</Link>
+          <Link className="text" href="tel:+380472361014"><FaxIcon/>+38-0472-361-014</Link>
+          <Link className="text" href="tel:+380472361014"><ContactPhoneIcon/>+38-0472-361-014</Link>
+          <Link className="text" href="tel:+380673956256"><PhoneIcon/>+38-067-395-62-56</Link>
+        </div>
+        <div className="mapContainer">
+          <h2>Мапа проїзду</h2>
+          <Map/>
+        </div>
+      </div>
+      
     </section>
   )
 }
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,7 +5,7 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("dark");
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
