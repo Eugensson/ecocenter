f92fb1977bd5099d5f0388dcaa6dbece32249be6
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2023-09-12 13:08:49 +0300

    Update servicesWater-page

--- src/app/servicesWater/page.jsx
+++ src/app/servicesWater/page.jsx
@@ -12,8 +12,7 @@ const ServicesWater = () => {
             <li className="navItem"><Link href="/waterGds" className="text"><CheckCircleIcon fontSize="small"/>Розробка проектів нормативів гранично-допустимих скидів</Link></li>
             <li className="navItem"><Link href="/waterNorms" className="text"><CheckCircleIcon fontSize="small"/>Розробка питомих норм водоспоживання та водовідведення, індивідуальних технологічних нормативів використання питної води</Link></li>
             <li className="navItem"><Link href="/waterPassport" className="text"><CheckCircleIcon fontSize="small"/>Розробка паспортів свердловин</Link></li>
-            <li className="navItem"><Link href="/waterZso" className="text"><CheckCircleIcon fontSize="small"/>Розробка проектів зон санітарної охорони джерел питного та господарсько-побутового водопостачання</Link></li>
-            <li className="navItem"><Link href="/waterPod" className="text"><CheckCircleIcon fontSize="small"/>Допомога у веденні журналів первинного обліку та звітної за фомами ПОД-11, ПОД-12 та ПОД-13</Link></li>
+            <li className="navItem"><Link href="/waterZso" className="text"><CheckCircleIcon fontSize="small"/>Розробка проектів зон санітарної охорони джерел питного та господарсько-побутового водопостачання</Link></li>            
             <li className="navItem"><Link href="/water2tp" className="text"><CheckCircleIcon fontSize="small"/>Звітність за формою № 2 тп - водгосп</Link></li>
           </ul>
           <LinkBtn text="До послуг" url="/services"/>
